0ba1f3634eaac882ed48c58066de085f268118c1
Podexus23 <santon23@gmail.com>
2023-08-21 22:47:01 +0300

    feat: add hashchange event

--- src/js/controller.js
+++ src/js/controller.js
@@ -32,9 +32,14 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+    if (!id) return;
+
+    // 1) Loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886?key=${api_key}`
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}?key=${api_key}`
     );
     const data = await res.json();
 
@@ -159,4 +164,9 @@ const showRecipe = async function () {
     console.error(error);
   }
 };
-showRecipe();
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
+// showRecipe();
