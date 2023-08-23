61e00e5cd4cb3f60c3fc8fdc144bcb9a06cc7190
Podexus23 <santon23@gmail.com>
2023-08-23 19:42:14 +0300

    feat: add search results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,19 +1,21 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import SearchView from './views/searchVies.js';
+import ResultsView from './views/resultsView.js';
 
 import 'regenerator-runtime';
 import 'core-js/stable';
+import resultsView from './views/resultsView.js';
 
-const recipeContainer = document.querySelector('.recipe');
-
-// https://forkify-api.herokuapp.com/v2
+if (module.hot) {
+  module.hot.accept();
+}
 
 ///////////////////////////////////////
 
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
     if (!id) return;
     recipeView.renderSpinner();
 
@@ -28,8 +30,27 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+    // 1) Get search query
+    const query = SearchView.getQuery();
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    // 3) Render results
+    console.log(model.state);
+    resultsView.render(model.state.search.results);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  SearchView.addHandlerSearch(controlSearchResults);
 };
 init();
 // window.addEventListener('hashchange', showRecipe);
--- src/js/model.js
+++ src/js/model.js
@@ -6,11 +6,15 @@ const api_key = `d5dc34bb-1546-4f27-a4eb-e070224fd794`;
 
 export const state = {
   recipe: {},
+  search: {
+    query: ``,
+    results: [],
+  },
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}${id}?key=${api_key}`);
+    const data = await getJSON(`${API_URL}/${id}?key=${api_key}`);
     const { recipe } = data.data;
     state.recipe = {
       id: recipe.id,
@@ -26,5 +30,24 @@ export const loadRecipe = async function (id) {
   } catch (error) {
     console.error(`model error: ${error} 💥`);
     throw error;
+  }
+};
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+
+    const data = await getJSON(`${API_URL}?search=${query}&key=${api_key}`);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+  } catch (error) {
+    console.log(`${err} error in search 💥💥`);
+    throw new Error(error);
   }
 };
