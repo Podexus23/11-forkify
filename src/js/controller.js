6080f387d751af09adc30a206302b6e137d41dc9
Podexus23 <santon23@gmail.com>
2023-08-24 22:43:01 +0300

    feat: add child class to resuls and bookmarks

--- src/js/controller.js
+++ src/js/controller.js
@@ -5,6 +5,7 @@ import * as model from './model.js';
 import searchView from './views/searchVies.js';
 import recipeView from './views/recipeView.js';
 import resultsView from './views/resultsView.js';
+import bookmarksView from './views/bookmarksView.js';
 import paginationView from './views/paginationView.js';
 
 // if (module.hot) {
@@ -21,6 +22,7 @@ const controlRecipes = async function () {
 
     // 0) results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     // 1) Loading recipe
     await model.loadRecipe(id);
@@ -66,12 +68,17 @@ const controlServings = function (newServings) {
 };
 
 const controlAddBookmark = function () {
+  //1) add or remove a bookmark
   if (model.state.recipe.bookmarked)
     model.deleteBookmark(model.state.recipe.id);
   else if (!model.state.recipe.bookmarked)
     model.addBookMark(model.state.recipe);
 
+  // 2) update recipe view
   recipeView.update(model.state.recipe);
+
+  // 3) Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const init = function () {
